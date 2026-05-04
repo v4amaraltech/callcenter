d585b4b60513581b81215acf6e973a1caece48d0
Kaique Nascimento <kaique.nascimento@v4company.com>
2026-05-04 17:20:52 -0300

    atualizações

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,9 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
+import ws from "ws"; // 👈 1. Importe o pacote ws aqui
 
 const key = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   key,
-  { auth: { persistSession: false } }
+  { 
+    auth: { persistSession: false },
+    realtime: { transport: ws } // 👈 2. Adicione o transport aqui
+  }
 );
